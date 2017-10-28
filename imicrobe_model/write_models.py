b0020f6a4d04eee81c9ac7613527489625753906
Joshua Lynch <jklynch@email.arizona.edu>
2017-10-28 16:43:28 -0400

    refactor for inheritance

--- imicrobe_model/write_models.py
+++ imicrobe_model/write_models.py
@@ -3,6 +3,7 @@ import collections
 import io
 import os
 import pprint
+import re
 
 import sqlalchemy as sa
 
@@ -35,6 +36,7 @@ class ModelWriter():
     def import_model_base(self):
         return """\
 import sqlalchemy as sa
+import sqlalchemy.dialects.mysql as mysql
 from sqlalchemy.ext.declarative import declarative_base
 
 Model = declarative_base()
@@ -44,34 +46,46 @@ Model = declarative_base()
     def get_additional_imports(self):
         return ''
 
-    def write_json_method(self):
-        return False
+    name_translations = {
+        re.compile(r'class'): 'class_'
+    }
 
-    def find_fk_constraint_by_column_name(self, column_names, fk_constraints):
-        """
-        Given a list of column names and a list of foreign key constaint
-        dictionaries, return a list of foreign key constaint dictionaries
-        corresponding to the column names (in the same order). If a column
-        name does not have a foreign key constraint return None in its place.
-        """
-        found_fk_contraints = []
-        # return fk_constraints in the same order as column_names
-        for column_name in column_names:
-            found_fk_contraint = None
-            for fk_constraint in fk_constraints:
-                # TODO: what if more than one constrained column?
-                if len(fk_constraint['constrained_columns']) > 1:
-                    raise Exception()
-                elif fk_constraint['constrained_columns'][0] == column_name:
-                    found_fk_contraint = fk_constraint
-                    break
-                else:
-                    # keep looking
-                    pass
-            # found_fk_contraint may be None and that is ok
-            found_fk_contraints.append(found_fk_contraint)
+    def translate_column_name_to_py(self, column_name):
+        for pattern, translation in self.name_translations.items():
+            if pattern.fullmatch(column_name):
+                return pattern.sub(string=column_name, repl=translation)
+            else:
+                pass
+        # if no translation was made return the input unchanged
+        return column_name
 
-        return tuple(found_fk_contraints)
+    type_translations = {
+        re.compile(r'BIGINT\(\d+\) UNSIGNED'): r'mysql.BIGINT(unsigned=True)',
+        re.compile(r'DATE'): r'sa.Date',
+        re.compile(r'DATETIME'): r'sa.DateTime',
+        re.compile(r'DOUBLE'): r'sa.Float',
+        re.compile(r'ENUM\((.+)\)'): r'mysql.ENUM(\1)',
+        re.compile(r'INTEGER\(\d+\)'): r'mysql.INTEGER()',
+        re.compile(r'INTEGER\(\d+\) UNSIGNED'): r'mysql.INTEGER(unsigned=True)',
+        re.compile(r'LONGTEXT'): r'mysql.LONGTEXT()',
+        re.compile(r'MEDIUMTEXT'): r'mysql.MEDIUMTEXT()',
+        re.compile(r'TEXT'): r'mysql.TEXT()',
+        re.compile(r'TIMESTAMP'): r'mysql.TIMESTAMP',
+        re.compile(r'TINYINT\((\d+)\)'): r'mysql.TINYINT(\1)',
+        re.compile(r'VARCHAR\((\d+)\)'): r'mysql.VARCHAR(\1)'
+    }
+
+    def translate_column_type_to_sa(self, column_type):
+        for pattern, translation in self.type_translations.items():
+            if pattern.fullmatch(column_type):
+                return pattern.sub(string=column_type, repl=translation)
+            else:
+                pass
+        # if no translation was made return the input unchanged
+        return column_type
+
+    def write_additional_methods(self, table, table_code):
+        return
 
     def get_many_to_many_relations(self, table):
         insp = sa.engine.reflection.Inspector.from_engine(self.engine)
@@ -106,35 +120,22 @@ Model = declarative_base()
               'referred_schema': None,
               'referred_table': 'uproc'}]
         """
-        # find two-column unique constraints in which both columns
-        # are foreign keys -- this criterion may be too strict
-        """
+        #pprint.pprint(foreign_key_constraints)
         many_to_many_relations = []
-        for unique_constraint in insp.get_unique_constraints(table.name):
-            if len(unique_constraint['column_names']) == 2:
-                column_name_a, column_name_b = unique_constraint['column_names']
-                fk_constraint_a, fk_constraint_b = self.find_fk_constraint_by_column_name(
-                    (column_name_a, column_name_b), foreign_key_constraints)
-                if fk_constraint_a is None:
-                    pass
-                elif fk_constraint_b is None:
-                    pass
-                else:
-                    # both members of the unique constraint are also
-                    # foreign keys
-                    many_to_many_relations.append(
-                        (table.c[column_name_a], table.c[column_name_b]))
-        """
-        print('')
-        print(table.name)
-        pprint.pprint(foreign_key_constraints)
-        many_to_many_relations = []
+        # TODO: find a better way to identify an association table
+        # if this table has exactly 2 foreign keys then it is an association table
         if len(foreign_key_constraints) != 2:
             pass
         else:
             table_0 = self.meta.tables[foreign_key_constraints[0]['referred_table']]
             table_1 = self.meta.tables[foreign_key_constraints[1]['referred_table']]
-            many_to_many_relations.append((table_0, table_1))
+
+            table_0_to_table_1 = '{}_to_{}'.format(table_0.name, table_1.name)
+            table_1_to_table_0 = '{}_to_{}'.format(table_1.name, table_0.name)
+            if table.name == table_0_to_table_1 or table.name == table_1_to_table_0:
+                many_to_many_relations.append((table_0, table_1))
+            else:
+                pass
 
         return tuple(many_to_many_relations)
 
@@ -145,102 +146,64 @@ Model = declarative_base()
         # so we can go back to add relationships
         def string_io_factory():
             return io.StringIO()
+        table_to_table_code = collections.defaultdict(string_io_factory)
 
-        table_name_to_table_code = collections.defaultdict(string_io_factory)
-
+        insp = sa.engine.reflection.Inspector.from_engine(self.engine)
         for table in self.meta.sorted_tables:
             print(table)
 
-            table_code = table_name_to_table_code[table.name]
-            #table_code.write("class {}(db.Model):\n".format(table.name.capitalize()))
+            table_code = table_to_table_code[table]
 
             # class Table_name(Model):
             table_code.write("class {}({}):\n".format(
                 table.name.capitalize(),
                 self.get_model_parent_class_name()))
 
-            table_code.write("    __tablename__ = '{}'\n".format(table.name))
+            table_code.write("    __tablename__ = '{}'\n\n".format(table.name))
 
-            for column in table.columns:
-                print('table {} has column {} with type {}'.format(
-                    table.name, column.name, column.type))
-
-                column_name = column.name
-                column_attribute_name = column.name
-                column_type = str(column.type).split()[0]
-                column_modifiers = []
-
-                print('column_name: {}'.format(column_name))
-                print('column_type: {}'.format(column_type))
-
-                if column.primary_key:
-                    column_type = 'Integer'
-                    column_modifiers.append(', primary_key=True')
-                elif column_name.endswith('_id'):
-                    # assume this column is a foreign key
-                    column_type = 'Integer'
-                    column_modifiers.append(
-                        ', sa.ForeignKey(\'{}.{}\')'.format(
-                            column_name[:-3], column_name))
-                elif column.name == 'class':
-                    column_attribute_name = 'class_'
-                    column_name = 'class'
+            # handle foreign keys with explicit ForeignKeyConstraints
+            foreign_key_constraints = [
+                    "        sa.ForeignKeyConstraint([{}], [{}])".format(
+                        ','.join( ["'{}'".format(c_) for c_ in fk_constraint['constrained_columns']] ),
+                        ','.join( ["'{}.{}'".format(fk_constraint['referred_table'], r_) for r_ in fk_constraint['referred_columns']] ))
+                    for fk_constraint
+                    in insp.get_foreign_keys(table_name=table.name)]
+            if len(foreign_key_constraints) == 0:
+                pass
+            else:
+                table_code.write('    __table_args__ = (\n')
+                table_code.write(',\n'.join(foreign_key_constraints))
+                if len(foreign_key_constraints) == 1:
+                    # write the trailing ',' for a 1-ple
+                    table_code.write(',')
                 else:
                     pass
+                table_code.write('\n    )\n\n')
 
-                # TODO: need different names for model attribute and db column
-                # because 'class' is a keyword
-
-                # this is so ugly
-                if column_type.startswith('INTEGER'):
-                    column_type = 'INTEGER'
-                elif column_type.startswith('BIGINT'):
-                    column_type = 'INTEGER'
-                elif column_type.startswith('TINYINT'):
-                    column_type = 'INTEGER'
-                elif column_type.startswith('DOUBLE'):
-                    column_type = 'Float'
-                # TODO: what should LONGTEXT map to?
-                # TODO: what should MEDIUMTEXT map to?
-                elif column_type.startswith('LONGTEXT'):
-                    column_type = 'Text'
-                elif column_type.startswith('MEDIUMTEXT'):
-                    column_type = 'Text'
-                elif column_type.startswith('ENUM'):
-                    column_type = 'Enum'
-                else:
-                    pass
-
+            # handle primary key columns
+            for pk_column in (table.c[c_] for c_ in insp.get_pk_constraint(table_name=table.name)['constrained_columns']):
                 table_code.write(
-                    "    {} = sa.Column('{}', sa.{}{})\n".format(
-                        column_attribute_name,
-                        column_name,
-                        column_type, ''.join(column_modifiers)))
+                    "    {} = sa.Column('{}', {}, primary_key=True)\n".format(
+                        self.translate_column_name_to_py(pk_column.name),
+                        pk_column.name,
+                        self.translate_column_type_to_sa(str(pk_column.type))))
+
+            table_code.write('\n')
+
+            # handle the data columns including foreign key columns
+            for column in table.columns:
+                if column.primary_key:
+                    pass
+                else:
+                    table_code.write(
+                        "    {} = sa.Column('{}', {})  # column.type was '{}'\n".format(
+                            self.translate_column_name_to_py(column.name),
+                            column.name,
+                            self.translate_column_type_to_sa(str(column.type)),
+                            column.type))
+                    table_code.write('\n')
 
             table_code.write("\n")
-
-            if self.write_json_method():
-                table_code.write("    def json(self):\n        return {\n")
-                for column in table.columns:
-                    column_name = column.name
-                    column_attribute_name = column.name
-                    if column.primary_key:
-                        # do not include primary key in JSON
-                        continue
-                    elif column.name == 'class':
-                        column_name = 'class'
-                        column_attribute_name = 'class_'
-                    else:
-                        pass
-
-                    table_code.write(
-                        "            '{}': self.{},\n".format(
-                            column_name, column_attribute_name))
-                table_code.write("        }")
-                table_code.write("\n\n")
-
-            # print(dir(table))
-            print(table.columns)
 
         # add relationships
         relationship_code = """\
@@ -255,7 +218,7 @@ Model = declarative_base()
             for (table_a, table_b) in self.get_many_to_many_relations(table):
                 print('  assume table "{}" represents a many-to-many relationship between tables "{}" and "{}"'.format(table.name, table_a.name, table_b.name))
 
-                table_a_code = table_name_to_table_code[table_a.name]
+                table_a_code = table_to_table_code[table_a]
                 table_a_code.write(
                     relationship_code.format(
                         table_1=table_a.name,
@@ -263,7 +226,7 @@ Model = declarative_base()
                         relationship_table=table.name,
                         table_2=table_b.name))
 
-                table_b_code = table_name_to_table_code[table_b.name]
+                table_b_code = table_to_table_code[table_b]
                 table_b_code.write(
                     relationship_code.format(
                         table_1=table_b.name,
@@ -272,13 +235,16 @@ Model = declarative_base()
                         table_2=table_a.name))
 
 
+        for table, table_code in table_to_table_code.items():
+            self.write_additional_methods(table, table_code)
+
         with open(output_fp, 'wt') as test_models:
             test_models.write(self.import_model_base())
             test_models.write(self.get_additional_imports())
 
             test_models.write(self.get_additional_imports())
             ##flask: test_models.write("from app import db\n\n")
-            for _, table_code in sorted(table_name_to_table_code.items()):
+            for _, table_code in sorted(table_to_table_code.items(), key=lambda k_v: k_v[0].name):
                 test_models.write(table_code.getvalue())
                 test_models.write('\n')
 
